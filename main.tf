4f083ad90552dba69eab31f48b0fc5a1044cbae2
catchabhiagg <catchabihagg@gmail.com>
2026-04-18 00:39:46 +0530

    Fix ip allocation

--- main.tf
+++ main.tf
@@ -51,7 +51,8 @@ resource "azurerm_public_ip" "pip" {
   name                = "my-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 # NIC
