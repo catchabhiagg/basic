c0f254aff79f2c7bcdfb74028ee4412ddcc3acd6
catchabhiagg <catchabihagg@gmail.com>
2026-04-18 01:03:04 +0530

    merge dev into main

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "my-vnet-dev"
+  name                = "my-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
